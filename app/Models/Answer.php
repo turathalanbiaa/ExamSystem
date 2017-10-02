d42a06272e3dab0f81a6919ca1fbbd9778911bb7
Ali Faris <alihcompiler@gmail.com>
2017-10-02 10:12:01 +0300

    adding Answer , Exam , Option , Question Models

---
+++ app/Models/Answer.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ali
+ * Date: 10/2/17
+ * Time: 10:10 AM
+ */
+
+namespace App\Models;
+
+
+class Answer
+{
+
+}
---
+++ app/Models/Option.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ali
+ * Date: 10/2/17
+ * Time: 10:11 AM
+ */
+
+namespace App\Models;
+
+
+class Option
+{
+
+}
