6115e740ae54ad6fd1c9175369b1ff88bf9b7b20
Ali Faris <alihcompiler@gmail.com>
2017-10-02 11:20:31 +0300

    get all exam & enroll on exam

--- app/Models/Exam.php
+++ app/Models/Exam.php
@@ -4,6 +4,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Exam extends Model
 {
@@ -15,4 +16,24 @@ class Exam extends Model
     {
         return $this->hasMany('App\Models\Question' , "Exam_ID" , "ID");
     }
+
+    public function enroll(User $user)
+    {
+        $enrollment = new ExamEnrollment();
+        $enrollment->Exam_ID = $this->ID;
+        $enrollment->User_ID = $user->ID;
+        $enrollment->Mark = null;
+        $enrollment->Status = 0;
+        return $enrollment->save();
+    }
+
+    public function didEnrolled(User $user)
+    {
+        $enrollment = ExamEnrollment::where("Exam_ID" , $this->ID)->where("User_ID" , $user->ID)->first();
+        if ($enrollment)
+            return true;
+
+        return false;
+    }
+
 }
