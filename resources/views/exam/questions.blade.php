c9fad422e058ada03f0fee8d7835eafa07f5ac97
karrar <karrar@email.com>
2018-02-04 12:52:48 +0300

    فهىغ ؤاشىلث 2

--- resources/views/exam/questions.blade.php
+++ resources/views/exam/questions.blade.php
@@ -1,16 +1,16 @@
 <?php $i = 0; ?>
 
-{{--<div class="column">--}}
-    {{--<h2 class="ui top attached center aligned black inverted header">س١/ صح او خطأ - اجب على {{$exam->CategoryOne}} فرع</h2>--}}
-    {{--<div class="ui attached segment">--}}
+<div class="column">
+    <h2 class="ui top attached center aligned black inverted header">القسم الاول/اجب على {{$exam->CategoryOne}} فرع</h2>
+    <div class="ui attached segment">
 
-        {{--@foreach($trueOrFalseQuestions as $question)--}}
-            {{--@include('exam.question' , ["question" => $question , "i" => ++$i])--}}
-        {{--@endforeach--}}
+        @foreach($trueOrFalseQuestions as $question)
+            @include('exam.question' , ["question" => $question , "i" => ++$i])
+        @endforeach
 
-    {{--</div>--}}
+    </div>
 
-{{--</div>--}}
+</div>
 
 <div class="ui hidden divider"></div>
 <div class="ui divider" style="width: 100%;"></div>
@@ -19,11 +19,9 @@
 <?php $i = 0; ?>
 
 <div class="column">
-    <h2 class="ui top attached center aligned black inverted header"> اجب على {{$exam->CategoryTwo}} فرع</h2>
+    <h2 class="ui top attached center aligned black inverted header">القسم الثاني - اجب على {{$exam->CategoryTwo}} فرع</h2>
     <div class="ui attached segment">
-        @foreach($trueOrFalseQuestions as $question)
-            @include('exam.question' , ["question" => $question , "i" => ++$i])
-        @endforeach
+
         @foreach($optionsQuestions as $question)
             @include('exam.question' , ["question" => $question , "i" => ++$i])
         @endforeach
